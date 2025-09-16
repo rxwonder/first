b2b5e7efea8911bcd6cd16fc10dfa82d51d939b9
Adill <iidragonbustii@gmail.com>
2025-09-16 18:57:14 +0600

    testing

--- finance_project.cpp
+++ finance_project.cpp
@@ -1,13 +1,20 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
 class finance{
     private:
     int balance = 0;
+    string owner_name;
+    int owner_id;
     vector<int> data;
+    static vector<int> clients_id;
+    
     public:
-    string option;
+    
     void calculate_procent(){
         cout<<"How many operations?"<<endl;
         int size;
@@ -24,13 +31,45 @@ class finance{
         }
         int tool = balance / 100;
         for(int i = 0; i < size; i++){
-            cout<<"for: "<<data[i]<<" "<<(double)data[i] / tool<<endl;
+            double wastes = 0;
+            wastes = (double)data[i] / tool;
+            if(data[i] / 100 >= 0){
+            cout<<"for: "<<data[i]<<"\t";
+            string graph = draw_percent(wastes);
+            cout<<graph<<endl;
+            continue;
+        } if(data[i] / 10 >= 0) {
+            cout<<"for: "<<data[i]<<" \t";
+            string graph = draw_percent(wastes);
+            cout<<graph<<endl;
+            continue;
+        }else{
+            continue;
+            cout<<"for: "<<data[i]<<"  \t";
+            string graph = draw_percent(wastes);
+            cout<<graph<<endl;
+        }
 
         }
     }
+    string draw_percent(double wastes) {
+    string result;
+    int count = (int)(wastes / 2);
+    for(int i = 0; i < count; i++) {
+        result += "#";
+    }
+    return result;
+}
+    void show_sum(){
+        cout<<balance<<endl;
+    }
 };
+
+
 int main(){
+    srand(time(NULL));
     finance objA;
     objA.calculate_procent();
+    objA.show_sum();
     return 0;
 }
